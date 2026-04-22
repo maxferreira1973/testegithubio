3a992ec12ed3851383c9e0a9fcbdfb29094f56cb
vboxuser <max@ferreira.pro.br>
2026-04-22 11:19:02 -0300

    Correćão tagueamento.js apagado

---
+++ js/tagueamento.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const pageLoc = window.location.href;
+
+    // --- EVENTS MENU ---
+    const menuContato = document.querySelector('.menu-lista-contato');
+    if (menuContato) {
+        menuContato.addEventListener('click', () => {
+            gtag('event', 'click', { 
+                'page_location': pageLoc, 
+                'element_name': 'entre_em_contato', 
+                'element_group': 'menu' 
+            });
+        });
+    };
+}  
