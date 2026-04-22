29167d420217609780a3e9658635004d9bf6fddd
vboxuser <max@ferreira.pro.br>
2026-04-22 11:30:01 -0300

    Correćão tagueamento.js nome alternativo menu-click

--- js/tagueamento.js
+++ js/tagueamento.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const menuContato = document.querySelector('.menu-lista-contato');
     if (menuContato) {
         menuContato.addEventListener('click', () => {
-            gtag('event', 'click', { 
+            gtag('event', 'menu-click', { 
                 'page_location': pageLoc, 
                 'element_name': 'entre_em_contato', 
                 'element_group': 'menu' 
