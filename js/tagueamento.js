a781e608e77df339769cdb74f42a61ff96373474
vboxuser <max@ferreira.pro.br>
2026-04-23 09:57:11 -0300

    Chamada GA4 .js

--- js/tagueamento.js
+++ js/tagueamento.js
@@ -1,3 +1,23 @@
+// 1. Defina o seu ID de Medição
+const GA_MEASUREMENT_ID = 'G-RNW68HFLW1';
+
+// 2. Crie e injete o script da biblioteca do Google Tag Manager
+const script = document.createElement('script');
+script.src = `https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`;
+script.async = true;
+document.head.appendChild(script);
+
+// 3. Inicialize o dataLayer e a função gtag
+window.dataLayer = window.dataLayer || [];
+function gtag() {
+  window.dataLayer.push(arguments);
+}
+
+// 4. Configure os comandos iniciais
+gtag('js', new Date());
+gtag('config', GA_MEASUREMENT_ID, { 
+  'debug_mode': true 
+});
 document.addEventListener('DOMContentLoaded', () => {
     const pageLoc = window.location.href;
 
